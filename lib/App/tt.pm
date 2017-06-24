913cd0d503afae5062b5314dce8116bff97ec1d0
Jan Henning Thorsen <jhthorsen@cpan.org>
2017-06-24 10:19:37 +0200

    Released version 0.05
     - Add support for "tt edit" or "tt edit <date>"

--- lib/App/tt.pm
+++ lib/App/tt.pm
@@ -2,7 +2,7 @@ package App::tt;
 use strict;
 use warnings;
 
-our $VERSION = '0.04';
+our $VERSION = '0.05';
 
 1;
 
@@ -14,7 +14,7 @@ App::tt - Time tracking application
 
 =head1 VERSION
 
-0.04
+0.05
 
 =head1 DESCRIPTION
 
