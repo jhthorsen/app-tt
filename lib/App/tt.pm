c9f6558f41b218ceffc9da3c88dab11340b33311
Jan Henning Thorsen <jhthorsen@cpan.org>
2017-10-16 09:30:03 +0200

    Released version 0.06
     - Fix sorting "tt log" dates

--- lib/App/tt.pm
+++ lib/App/tt.pm
@@ -2,7 +2,7 @@ package App::tt;
 use strict;
 use warnings;
 
-our $VERSION = '0.05';
+our $VERSION = '0.06';
 
 1;
 
@@ -14,7 +14,7 @@ App::tt - Time tracking application
 
 =head1 VERSION
 
-0.05
+0.06
 
 =head1 DESCRIPTION
 
