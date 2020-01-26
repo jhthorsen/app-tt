2c3dec72368fb32a0b82d39bf1ad2d2e221681d1
Jan Henning Thorsen <jhthorsen@cpan.org>
2020-01-27 08:10:59 +0900

    Checking TIMETRACKER_MIN_TIME inside _save() instead of command_register()

--- lib/App/tt.pm
+++ lib/App/tt.pm
@@ -31,9 +31,9 @@ Here is a list of example usages, but you can get more details by adding "-h"
 after each command.
 
   # Register forgotten time
-  $ tt register 1983-01-10T09:00:00 17:00:00 "project-name"
-  $ tt register 1983-01-10T09:00:00 17:00:00 "project-name" "" "tag1,tag2"
-  $ tt register 1983-01-10T09:00:00 17:00:00 "project-name" "description" "tag1,tag2"
+  $ tt register 2020-01-01T09:00:00 17:00:00 "project-name"
+  $ tt register 2020-01-01T09:00:00 17:00:00 "project-name" "" "tag1,tag2"
+  $ tt register 2020-01-01T09:00:00 17:00:00 "project-name" "description" "tag1,tag2"
 
   # Edit the last entry, or a specific file
   $ tt edit
@@ -125,7 +125,7 @@ This command is used to import data from other sources. "project-name" default t
 "-p" or current git project, "some description" default to "-d" and tags can be
 specified by -t foo -t bar
 
-  $ tt register 2016-06-28T09:00:00 17:00:00 -p project-name -d "some description" -t foo -t bar
+  $ tt register 2020-01-01T09:00:00 17:00:00 -p project-name -d "some description" -t foo -t bar
 
 =head2 start
 
