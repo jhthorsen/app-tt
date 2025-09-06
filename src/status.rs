1c16d695a2211d8bceeebcb6b67e120b7acadfbf
Jan Henning Thorsen <jan.henning@thorsen.pm>
2025-09-06 17:46:27 +0900

    Implemented "status" subcommand

--- src/status.rs
+++ src/status.rs
@@ -1,6 +1,33 @@
+use crate::{entries::find_tracked_entries, utils::format_hour_minutes};
+use chrono::Datelike;
 use clap::Command;
 
 pub fn run(_args: &clap::ArgMatches) -> Result<i32, anyhow::Error> {
+    let now = chrono::Local::now().date_naive();
+    let first_of_month = now.with_day(1).expect("Invalid day");
+
+    let tracked_entries = find_tracked_entries(&first_of_month, &now);
+    let Some(last) = tracked_entries.last() else {
+        eprintln!("Unable to find the last tracked entry.");
+        return Ok(1);
+    };
+
+    if last.stop.is_none() {
+        println!(
+            "Tracking {} for {} since {}.",
+            last.project,
+            last.stop.unwrap().format("%Y-%m-%d %H:%M"),
+            format_hour_minutes(&last.duration()),
+        );
+    } else {
+        println!(
+            "Stopped tracking {} at {} after {}.",
+            last.project,
+            last.stop.unwrap().format("%Y-%m-%d %H:%M"),
+            format_hour_minutes(&last.duration()),
+        );
+    }
+
     Ok(0)
 }
 
